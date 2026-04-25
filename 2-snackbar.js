9fbc28554b6ad272cdb145c1f18316e6329e4971
daletskyiv <63199353+daletskyiv@users.noreply.github.com>
2026-04-25 18:19:15 +0000

    Deploying to gh-pages from @ daletskyiv/goit-js-hw-10@0ec1760070f30750218ad7a6b14fe8f26b039ef4 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const e={delayInput:document.querySelector('[name="delay"]'),stateSelectionBtn:document.querySelectorAll('[name="state"]'),submitBtn:document.querySelector(".submit-btn")};console.log(e);
+//# sourceMappingURL=2-snackbar.js.map
