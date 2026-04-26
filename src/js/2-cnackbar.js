ebf60b0b53b11be26db4c2d2230a4986b2ca545b
daletskyi_v <daletskyiv@gmail.com>
2026-04-26 15:38:04 +0300

    add html, css and js to snackbar section

--- src/js/2-cnackbar.js
+++ src/js/2-cnackbar.js
@@ -2,9 +2,40 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
-  delayInput: document.querySelector('[name="delay"]'),
-  stateSelectionBtn: document.querySelectorAll('[name="state"]'),
-  submitBtn: document.querySelector('.submit-btn'),
+  generatorForm: document.querySelector('.form'),
 };
 
-console.log(refs);
+refs.generatorForm.addEventListener('submit', onPromiseGeneratorSubmit);
+
+function onPromiseGeneratorSubmit(event) {
+  event.preventDefault();
+
+  const delay = event.target.elements.delay.value;
+  const value = event.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+
+  promise
+    .then(() => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(() => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  refs.generatorForm.reset();
+}
